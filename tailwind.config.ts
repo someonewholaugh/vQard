b9f0e9bd3c05b62080326c08284f80aa7e5273b1
Rafif Athallah <rafifathallah99@gmail.com>
2024-12-19 17:35:05 +0700

    feat(styles): update index.css and tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,11 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      colors: {
+        'black': '#121212',
+        'black-secondary': '#242424',
+        'white': '#faf9f5',
+      },
       fontFamily: {
         'projekt-blackbird': ['Projekt Blackbird', 'sans-serif'],
       },
